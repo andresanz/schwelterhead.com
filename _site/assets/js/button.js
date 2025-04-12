0211fdc80ec57b78c67475e1d6aafc0d253b907c
andresanz <sanz.andre@gmail.com>
2025-04-11 22:07:54 -0400

    overwriting again

---
+++ _site/assets/js/button.js
@@ -0,0 +1,15 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  const backToTopBtn = document.getElementById("backToTopBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopBtn.classList.add("show");
+  } else {
+    backToTopBtn.classList.remove("show");
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
